5d9a95ed093732e1e8a86e7d0e54af36b7508a69
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-17 17:45:36 -0300

    team statistics

---
+++ api-football/Models/Statistics/StatisticsTeam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Statistics
+{
+    internal class StatisticsTeam
+    {
+        public int id { get; set; }
+        public required string name { get; set; }
+        public required string logo { get; set; }
+    }
+}
