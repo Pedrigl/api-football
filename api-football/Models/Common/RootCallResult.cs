7e1f42d4a685f89d935d1eb786abe2e0bb42b7a3
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-17 16:10:45 -0300

    adding the root api call result

---
+++ api-football/Models/Common/RootCallResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Common
+{
+    internal class RootCallResult<T> where T : class
+    {
+        public required string get { get; set; }
+        public dynamic? parameters { get; set; }
+        public dynamic? errors { get; set; }
+        public int results { get; set; }
+        public required Paging paging { get; set; }
+        public List<T>? response { get; set; }
+    }
+}
