278cad0e99621e9c6e11da1dbbd08b4c66589c0b
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-17 17:08:13 -0300

    adding league and season models

---
+++ api-football/Models/League/League.cs
@@ -0,0 +1,16 @@
+﻿using api_football.Models.Season;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.League
+{
+    internal class League
+    {
+        public required LeagueInfo league { get; set; }
+        public required Country country { get; set; }
+        public required SeasonInfo[] season { get; set; }
+    }
+}
---
+++ api-football/Models/League/LeagueInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.League
+{
+    internal class LeagueInfo
+    {
+        public int id { get; set; }
+        public required string name { get; set; }
+        public required string type { get; set; }
+        public required string logo { get; set; }
+    }
+}
