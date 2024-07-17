d016ecb12ec197c37514f29dded27a6fe3e15a17
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-17 17:44:27 -0300

    league model for statistics

---
+++ api-football/Models/Statistics/StatisticsLeague.cs
@@ -0,0 +1,19 @@
+﻿using api_football.Models.League;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Statistics
+{
+    internal class StatisticsLeague 
+    {
+        public int id { get; set; }
+        public required string name { get; set; }
+        public required string country { get; set; }
+        public required string logo { get; set; }
+        public required string flag { get; set; }
+        public int season { get; set; }
+    }
+}
