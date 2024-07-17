42d9de73d8f54efedf813a0de8e9230e08079c0c
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-17 16:21:09 -0300

    adding country model

---
+++ api-football/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using api_football.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models
+{
+    internal class Country
+    {
+        public required string name { get; set; }
+        public required string code { get; set; }
+        public required string flag { get; set; }
+    }
+}
