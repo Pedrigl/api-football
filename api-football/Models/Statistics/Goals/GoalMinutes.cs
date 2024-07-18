2f8c29edc8bbf7b0d0aeab072fab02dde090864d
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-18 09:29:15 -0300

    genericfication of the minute percentage

--- api-football/Models/Statistics/Goals/GoalMinutes.cs
+++ api-football/Models/Statistics/Goals/GoalMinutes.cs
@@ -10,27 +10,27 @@ namespace api_football.Models.Statistics.Goals.Goals
     internal class GoalMinutes
     {
         [JsonPropertyName("0-15")]
-        public required GoalMinuteDetails _0_15 { get; set; }
+        public required MinutePercentageDetails _0_15 { get; set; }
 
         [JsonPropertyName("16-30")]
-        public required GoalMinuteDetails _16_30 { get; set; }
+        public required MinutePercentageDetails _16_30 { get; set; }
 
         [JsonPropertyName("31-45")]
-        public required GoalMinuteDetails _31_45 { get; set; }
+        public required MinutePercentageDetails _31_45 { get; set; }
 
         [JsonPropertyName("46-60")]
-        public required GoalMinuteDetails _46_60 { get; set; }
+        public required MinutePercentageDetails _46_60 { get; set; }
 
         [JsonPropertyName("61-75")]
-        public required GoalMinuteDetails _61_75 { get; set; }
+        public required MinutePercentageDetails _61_75 { get; set; }
 
         [JsonPropertyName("76-90")]
-        public required GoalMinuteDetails _76_90 { get; set; }
+        public required MinutePercentageDetails _76_90 { get; set; }
 
         [JsonPropertyName("91-105")]
-        public required GoalMinuteDetails _91_105 { get; set; }
+        public required MinutePercentageDetails _91_105 { get; set; }
 
         [JsonPropertyName("106-120")]
-        public required GoalMinuteDetails _106_120 { get; set; }
+        public required MinutePercentageDetails _106_120 { get; set; }
     }
 }
--- api-football/Models/Statistics/MinutePercentageDetails.cs
+++ api-football/Models/Statistics/MinutePercentageDetails.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace api_football.Models.Statistics.Goals.Goals
+namespace api_football.Models.Statistics
 {
-    internal class GoalMinuteDetails
+    internal class MinutePercentageDetails
     {
         public int minute { get; set; }
         public required string percentage { get; set; }
