6f80717a4fc1a90a13e3c849ac6c705207cf4bbb
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-18 14:34:59 -0300

    root namespace

---
+++ api-football/Models/Root/Paging.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Root
+{
+    internal class Paging
+    {
+        public int current { get; set; }
+        public int total { get; set; }
+    }
+}
---
+++ api-football/Models/Root/RootCallResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Root
+{
+    internal class RootCallResult<T> where T : class
+    {
+        public required string get { get; set; }
+        public object? parameters { get; set; }
+        public object[]? errors { get; set; }
+        public int results { get; set; }
+        public required Paging paging { get; set; }
+        public List<T>? response { get; set; }
+    }
+}
