07af9a60b1ad0c23360c1ffab9b339daa38c0677
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-18 19:15:22 -0300

    squads

---
+++ api-football/Models/Players/Squads/Squad.cs
@@ -0,0 +1,15 @@
+﻿using api_football.Models.Statistics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Players.Squads
+{
+    public class Squad
+    {
+        public required Team team { get; set; }
+        public required SquadPlayer[] players { get; set; }
+    }
+}
---
+++ api-football/Models/Players/Squads/SquadPlayer.cs
@@ -0,0 +1,16 @@
+﻿using api_football.Models.Persons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Players.Squads
+{
+    public class SquadPlayer : PersonInfo
+    {
+        public int age { get; set; }
+        public int number { get; set; }
+        public required string position { get; set; }
+    }
+}
