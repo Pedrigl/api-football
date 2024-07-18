f23d151f57167fae8f1933223ad5ac9621c191c2
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-18 08:29:34 -0300

    statistics/goals namespace

--- api-football/Models/Statistics/Goals/GoalMinuteDetails.cs
+++ api-football/Models/Statistics/Goals/GoalMinuteDetails.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace api_football.Models.Statistics
+namespace api_football.Models.Statistics.Goals.Goals
 {
     internal class GoalMinuteDetails
     {
