ff129932946c76217c70c7bf088dababf00dcbfe
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-19 00:33:31 -0300

    dependency injection

---
+++ api-football/Extemsions/Http/ApiSportsClientBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Extemsions.Http
+{
+    internal class ApiSportsClientBuilder : IHttpClientBuilder
+    {
+        private readonly string _apiKey;
+
+        public ApiSportsClientBuilder(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public HttpClient Build()
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(" https://v3.football.api-sports.io/"),
+                DefaultRequestHeaders =
+                {
+                    { "x-spisports-key", _apiKey }
+                },
+            };
+            return client;
+        }
+    }
+}
