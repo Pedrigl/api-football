dd748ba4d4d070de695ecc0d0f12dd111b756c2d
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-19 01:14:15 -0300

    timezones handler

---
+++ api-football/Handlers/TimezoneHandler.cs
@@ -0,0 +1,29 @@
+﻿using api_football.Models.Root;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace api_football.Handlers
+{
+    public class TimezoneHandler
+    {
+        private readonly HttpClient _client;
+        public TimezoneHandler(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<RootCallResult<string[]>> GetTimezones()
+        {
+            var response = await _client.GetAsync("timezone");
+            var content = await response.Content.ReadAsStreamAsync();
+            if (response.StatusCode > System.Net.HttpStatusCode.NoContent)
+                throw new HttpRequestException((await JsonSerializer.DeserializeAsync<ErrorMessage>(content)).message);
+
+            return await JsonSerializer.DeserializeAsync<RootCallResult<string[]>>(content);
+        }
+    }
+}
---
+++ api-football/Models/Root/ErrorMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api_football.Models.Root
+{
+    public class ErrorMessage
+    {
+        internal ErrorMessage()
+        {
+        }
+        public required string message { get; set; }
+    }
+}
