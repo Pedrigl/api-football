3939baea80c2a3610807766d412cf5c9d85290df
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-21 15:59:55 -0300

    coach test

---
+++ tests/Models/Coaches/CoachTest.cs
@@ -0,0 +1,29 @@
+﻿using api_football.Models.Coaches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tests.Models.Coaches
+{
+    [TestClass]
+    public class CoachTest
+    {
+        private string _coachJson;
+
+        [TestInitialize]
+        private void Setup()
+        {
+            var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        [TestMethod]
+        public void TestCoach()
+        {
+
+
+        }
+
+    }
+}
