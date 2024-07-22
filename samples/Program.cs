935fe3d7528bbbd6309a462ca746d0fe039baeaa
Pedro <pedrogabrielcruzlima@gmail.com>
2024-07-22 00:30:59 -0300

    samples project

---
+++ samples/Program.cs
@@ -0,0 +1,7 @@
+﻿using api_football.Extensions;
+using api_football.Extensions.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+
+services.AddApiFootballServices(new ApiSportsClientBuilder("20fbdf493eb459564d090a9583f4085a", services.))
